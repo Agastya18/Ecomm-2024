10eb9323fd5f063f645408980b56b30cd17a2548
agastya gaur <agastyagaur17@gmail.com>
2024-01-17 13:33:32 +0530

    Add product data and API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import products from './data/product.js';
 dotenv.config(
     // {
     //     path: './.env'
@@ -8,8 +8,19 @@ dotenv.config(
 );
 const app = express();
 const PORT=process.env.PORT
+
+//middlewares
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
-  res.send('Server is ready');
+  res.json(products);
+})
+app.get('/api/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  console.log(product);
+  res.json(product);
 })
 
 app.listen(PORT, () => {
