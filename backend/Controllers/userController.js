1f04d15df5140eadd263e42ed2440c1037b00543
agastya gaur <agastyagaur17@gmail.com>
2024-01-18 19:15:34 +0530

    Add userController and userRoute files

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,5 @@
+export const userRegister = async(req,res)=>{
+   console.log("testing user register")
+}
+
+
--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,29 @@ import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/product.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 dotenv.config(
-    // {
-    //     path: './.env'
-    // }
+     {
+         path: './.env'
+     }
 );
 const app = express();
 const PORT=process.env.PORT
 connectDB();
+
 //middlewares
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
+//routes
 app.get('/', (req, res) => {
   res.json(products);
 })
-app.get('/api/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  console.log(product);
-  res.json(product);
-})
+import userRoute from './routes/userRoute.js';
+
+app.use('/api/v1', userRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Serve at http://localhost:${process.env.PORT}`);
