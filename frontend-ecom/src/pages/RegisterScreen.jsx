9cdca25a1f6e103abcf702c2e973242fd384fb9e
agastya gaur <agastyagaur17@gmail.com>
2024-01-19 22:00:07 +0530

    Add login screen design and logo

--- frontend-ecom/src/pages/RegisterScreen.jsx
+++ frontend-ecom/src/pages/RegisterScreen.jsx
@@ -36,147 +36,195 @@ const RegisterScreen = () => {
 
 }
   return (
-    // <div className=' min-h-screen bg-gray-300 flex flex-col justify-center items-center'>
-    //     <div className=' bg-white px-16 pt-8 pb-12 mb-4'>
-    //           <h1 className=' text-3xl font-mono text-center'>Register user</h1>
-    //           <form >
-    //           <label  className=' block text-gray-600 mt-4'>Name:</label>
-    //             <input  label="Name"
-    //                 type="text"
-    //                 id="name"
-    //                 value={name}
-    //                 onChange={(e)=>setName(e.target.value)}
-    //                 className=' w-full p-2 border border-gray-300 rounded-md focus:outline-none '
-                   
-    //             />
-    //             <label  className=' block text-gray-600 mt-4'>Email: </label>
-    //              <input  label="Email"
-    //                 type="text"
-    //                 id="email"
-    //                value={email}
-    //                onChange={(e)=>setEmail(e.target.value)}
-    //                className=' w-full p-2 border border-gray-300 rounded-md focus:outline-none '
-    //             />
-    //             <label  className=' block text-gray-600 mt-4'>Password: </label>
-    //              <input label="password"
-    //                 type="password"
-    //                 id="password"
-    //                value={password}
-    //                onChange={(e)=>setPass(e.target.value)}
-    //                className=' w-full p-2 border border-gray-300 rounded-md focus:outline-none '
-    //             />
-                
-                
-    //             <button onClick={(e)=>handleSubmit(e)} className=' bg-green-600   rounded-md p-2 mt-3   w-full px-4'>signup</button>
-    //             <p className=' inline-block'> Already have an account?</p>
-    //             <Link to="/login" className=' text-blue-500 ml-2'>Login</Link>
-    //           </form>
-    //     </div>
-    // </div>
-    <div className="flex w-screen   flex-wrap text-slate-800 ">
-    <div className="relative hidden h-screen select-none flex-col justify-center bg-blue-600 text-center md:flex md:w-1/2">
-      <div className="mx-auto py-16 px-8 text-white xl:w-[40rem]">
-        <span className="rounded-full bg-white px-3 py-1 font-medium text-blue-600">
-          New Feature
-        </span>
-        <p className="my-6 text-3xl font-semibold leading-10">
-          Create animations with{" "}
-          <span className="mx-auto block w-56 whitespace-nowrap rounded-lg bg-orange-400 py-2 text-white">
-            drag and drop
-          </span>
-        </p>
-        <p className="mb-4">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
-          necessitatibus nostrum repellendus ab totam.
-        </p>
-        <a
-          href="#"
-          className="font-semibold tracking-wide text-white underline underline-offset-4"
-        >
-          Learn More
-        </a>
-      </div>
-      {/* <img class="mx-auto w-11/12 max-w-lg rounded-lg object-cover" src="/images/SoOmmtD2P6rjV76JvJTc6.png" /> */}
-    </div>
-    <div className="flex w-full flex-col md:w-1/2">
-      <div className="flex justify-center pt-12 md:justify-start md:pl-12">
-        <a href="#" className="text-2xl font-bold text-blue-600 flex">
-          {" "}
-          <img
-            className="mr-2 h-6 "
-            src={logo}
-            alt=""
-          />{" "} Kharidoo.com{" "}
-        </a>
-      </div>
-      <div className="my-auto mx-auto flex flex-col justify-center px-6 pt-8 md:justify-start lg:w-[28rem]">
-        <p className="text-center text-3xl font-bold md:text-left md:leading-tight">
-          Create your free account
-        </p>
+    
+    
+  //     <div className="flex w-screen   flex-wrap text-slate-800  ">
+  //   <div className="relative hidden h-screen select-none flex-col justify-center bg-blue-600 text-center md:flex md:w-1/2">
+  //     <div className="mx-auto py-16 px-8 text-white xl:w-[40rem]">
+  //       <span className="rounded-full bg-white px-3 py-1 font-medium text-blue-600">
+  //         New Feature
+  //       </span>
+  //       <p className="my-6 text-3xl font-semibold leading-10">
+  //         Create animations with{" "}
+  //         <span className="mx-auto block w-56 whitespace-nowrap rounded-lg bg-orange-400 py-2 text-white">
+  //           drag and drop
+  //         </span>
+  //       </p>
+  //       <p className="mb-4">
+  //         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+  //         necessitatibus nostrum repellendus ab totam.
+  //       </p>
+  //       <a
+  //         href="#"
+  //         className="font-semibold tracking-wide text-white underline underline-offset-4"
+  //       >
+  //         Learn More
+  //       </a>
+  //     </div>
+  //     {/* <img class="mx-auto w-11/12 max-w-lg rounded-lg object-cover" src="/images/SoOmmtD2P6rjV76JvJTc6.png" /> */}
+  //   </div>
+  //   <div className="flex w-full flex-col md:w-1/2">
+  //     <div className="flex justify-center pt-12 md:justify-start md:pl-12">
+  //       <a href="#" className="text-2xl font-bold text-blue-600 flex">
+  //         {" "}
+  //         <img
+  //           className="mr-2 h-6 "
+  //           src={logo}
+  //           alt=""
+  //         />{" "} Kharidoo.com{" "}
+  //       </a>
+  //     </div>
+  //     <div className="my-auto mx-auto flex flex-col justify-center px-6 pt-8 md:justify-start lg:w-[28rem]">
+  //       <p className="text-center text-3xl font-bold md:text-left md:leading-tight">
+  //         Create your free account
+  //       </p>
         
-        <button className="-2 mt-8 flex items-center justify-center rounded-md border px-4 py-1 outline-none ring-gray-400 ring-offset-2 transition hover:border-transparent hover:bg-black hover:text-white focus:ring-2">
-          <img
-            className="mr-2 h-5"
-            src={goo}
-            alt=""
-          />{" "}
-          Get started with Google
-        </button>
-        <div className="relative mt-8 flex h-px place-items-center bg-gray-200">
-          <div className="absolute left-1/2 h-6 -translate-x-1/2 bg-white px-4 text-center text-sm text-gray-500">
-            Or use email instead
-          </div>
-        </div>
-        <form className="flex flex-col items-stretch pt-3 md:pt-8">
-          <div className="flex flex-col pt-4">
-            <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input
-                type="text"
-                id="login-name"
-                className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                placeholder="Name"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col pt-4">
-            <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input
-                type="email"
-                id="login-email"
-                className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                placeholder="Email"
-              />
-            </div>
-          </div>
-          <div className="mb-4 flex flex-col pt-4">
-            <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input
-                type="password"
-                id="login-password"
-                className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                placeholder="Password (minimum 8 characters)"
-              />
-            </div>
-          </div>
-          <div className="block">
+  //       <button className="-2 mt-8 flex items-center justify-center rounded-md border px-4 py-1 outline-none ring-gray-400 ring-offset-2 transition hover:border-transparent hover:bg-black hover:text-white focus:ring-2">
+  //         <img
+  //           className="mr-2 h-5"
+  //           src={goo}
+  //           alt=""
+  //         />{" "}
+  //         Get started with Google
+  //       </button>
+  //       <div className="relative mt-8 flex h-px place-items-center bg-gray-200">
+  //         <div className="absolute left-1/2 h-6 -translate-x-1/2 bg-white px-4 text-center text-sm text-gray-500">
+  //           Or use email instead
+  //         </div>
+  //       </div>
+  //       <form className="flex flex-col items-stretch pt-3 md:pt-8">
+  //         <div className="flex flex-col pt-4">
+  //           <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+  //             <input
+  //               type="text"
+  //               id="login-name"
+  //               className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+  //               placeholder="Name"
+  //             />
+  //           </div>
+  //         </div>
+  //         <div className="flex flex-col pt-4">
+  //           <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+  //             <input
+  //               type="email"
+  //               id="login-email"
+  //               className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+  //               placeholder="Email"
+  //             />
+  //           </div>
+  //         </div>
+  //         <div className="mb-4 flex flex-col pt-4">
+  //           <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+  //             <input
+  //               type="password"
+  //               id="login-password"
+  //               className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+  //               placeholder="Password (minimum 8 characters)"
+  //             />
+  //           </div>
+  //         </div>
+  //         <div className="block">
             
-          <p className="mt-6 text-center font-medium md:text-left">
-          Already using Kharidoo.com?{" "}
-          <a href="#" className="whitespace-nowrap font-semibold text-blue-700">
-            Sign In here
-          </a>
+  //         <p className="mt-6 text-center font-medium md:text-left">
+  //         Already using Kharidoo.com?{" "}
+  //         <a href="#" className="whitespace-nowrap font-semibold text-blue-700">
+  //           Sign In here
+  //         </a>
+  //       </p>
+  //         </div>
+  //         <button
+  //           type="submit"
+  //           className="mt-6 rounded-lg bg-blue-600 px-4 py-2 text-center text-base font-semibold text-white shadow-md outline-none ring-blue-500 ring-offset-2 transition hover:bg-blue-700 focus:ring-2 md:w-32"
+  //         >
+  //           Sign up
+  //         </button>
+  //       </form>
+  //     </div>
+  //   </div>
+  // </div>
+  <div className="flex h-screen w-full items-center justify-center bg-gray-100">
+  <div className="w-full max-w-3xl overflow-hidden rounded-lg bg-white shadow-lg sm:flex">
+    <div
+      className="m-2 w-full rounded-2xl bg-gray-400 bg-cover bg-center text-white sm:w-2/5"
+      style={{ backgroundImage: `url(${logo})` }}
+    />
+    <div className="w-full sm:w-3/5">
+      <div className="p-8">
+        <h1 className="text-3xl font-black text-slate-700">Sign up</h1>
+        <p className="mt-2 mb-5 text-base leading-tight text-gray-600">
+          Create an account to get access to 1000+ Freebies
         </p>
+        <form className="mt-8">
+        <div className="relative mt-2 w-full">
+            <input
+              type="text"
+              id="name"
+              
+              className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+              placeholder=" name"
+            />
+            <label
+              htmlFor="name"
+              className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"
+            >
+              {" "}
+              Enter Your Name{" "}
+            </label>
           </div>
-          <button
+          <div className="relative mt-2 w-full">
+            <input
+              type="text"
+              id="email"
+             
+              className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+              placeholder=" "
+            />
+            <label
+              htmlFor="email"
+              className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"
+            >
+              {" "}
+              Enter Your Email{" "}
+            </label>
+          </div>
+          <div className="relative mt-2 w-full">
+            <input
+              type="text"
+              id="password"
+              className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+              placeholder=" "
+            />
+            <label
+              htmlFor="password"
+              className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"
+            >
+              {" "}
+              Enter Your Password
+            </label>
+          </div>
+          <input
+            className="mt-4 w-full cursor-pointer rounded-lg bg-blue-600 pt-3 pb-3 text-white shadow-lg hover:bg-blue-400"
             type="submit"
-            className="mt-6 rounded-lg bg-blue-600 px-4 py-2 text-center text-base font-semibold text-white shadow-md outline-none ring-blue-500 ring-offset-2 transition hover:bg-blue-700 focus:ring-2 md:w-32"
-          >
-            Sign up
-          </button>
+            defaultValue="Create account"
+          />
         </form>
+        <div className="mt-4 text-center">
+          <p className="text-sm text-gray-600">
+            Already have an account?{" "}
+            <a
+              href="#"
+              className="font-bold text-blue-600 no-underline hover:text-blue-400"
+            >
+              Sign in
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   </div>
+</div>
+
+    
   
 
   )
