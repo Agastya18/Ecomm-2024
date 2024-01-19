5e9ebabde629684cea98e9bb6fc6eb7b89c1e3c9
agastya gaur <agastyagaur17@gmail.com>
2024-01-19 11:51:25 +0530

    Remove commented out code in userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -10,7 +10,7 @@ import { Admin, verifyUser } from "../middleware/authMiddleware.js";
  router.route("/login").post(userLogin)
  router.route("/logout").post(verifyUser,userLogout)
  router.route("/me").get(verifyUser,getCurrentUser)
- //router.route("/current-user").get(verifyUser,getCurrentUser)
+ 
  router.route("/update-user").put(verifyUser,updateUserProfile)
  router.route("/:id").get(verifyUser,getUserById)
  router.route("/:id").put(verifyUser,Admin,updateUserRole)
