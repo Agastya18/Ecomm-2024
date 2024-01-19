03e57c2bddbf337796f55011d366893971d32373
agastya gaur <agastyagaur17@gmail.com>
2024-01-19 11:50:59 +0530

    Fix router indentation and update package dependencies

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,14 +3,14 @@ import { getCurrentUser, getUserById, updateUserProfile, updateUserRole, userLog
 import { Admin, verifyUser } from "../middleware/authMiddleware.js";
 
 
-const router = Router();
+ const router = Router();
 
 
  router.route("/register").post(userRegister)
  router.route("/login").post(userLogin)
  router.route("/logout").post(verifyUser,userLogout)
  router.route("/me").get(verifyUser,getCurrentUser)
- router.route("/current-user").get(verifyUser,getCurrentUser)
+ //router.route("/current-user").get(verifyUser,getCurrentUser)
  router.route("/update-user").put(verifyUser,updateUserProfile)
  router.route("/:id").get(verifyUser,getUserById)
  router.route("/:id").put(verifyUser,Admin,updateUserRole)
